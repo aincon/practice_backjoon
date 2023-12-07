bdaa01c781aa6da09d3ba5d952469095a869992c
aincon <152691123+aincon@users.noreply.github.com>
2023-12-08 00:40:25 +0900

    복기
    우선 알파벳만 나오면 알파벳을 전부 나열하려고 하는데, 일단 개노답
    관계성있는 2개만 나오면 dict쓰려고 하는데 솔직히 필요없음 -> 솔직히 그냥 dict 드럽게 못 쓰는데 억지로 쓸바에야 쓰지마셈
    이런 문제 많이 나오는데 나올 때마다 인터넷 쓰면 발전이 없는 게 아닐까

---
+++ 1157.py
@@ -0,0 +1,17 @@
+"""
+알파벳 대소문자로 된 단어가 주어지면,
+이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오.
+단, 대문자와 소문자를 구분하지 않는다.
+"""
+word = input().upper()
+word_lst = list(set(word))
+cnt = []
+
+for i in word_lst:
+    cnt.append(word.count(i))
+
+if cnt.count(max(cnt)) >= 2:
+    print("?")
+
+else:
+    print(word_lst[cnt.index(max(cnt))])
